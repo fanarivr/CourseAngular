22be371bbf9e8f2e1b6551f3c34432513ba9d367
m3rariunah <fmventura@unah.hn>
2023-07-05 23:21:15 -0600

    Mensaje de confirmación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+//corazón cerebro del component
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi Primera clase';
+  public counter: number = 10;
+
+  increaseby(value: number): void {//void no regresa nada
+    this.counter += value;
+  }
+  reset() {
+    this.counter = 10
+  }
 }
