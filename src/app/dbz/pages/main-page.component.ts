6e6eff20f3b77e2d2dcbbaec449a90b2afbb8b99
unknown <fmventura@unah.hn>
2023-07-13 10:05:33 -0600

    version juevs

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DbzService } from '../services/dbz.service';
+import { Character } from '../interfaces/character.interface';
 
 @Component({//ENRUTAMIENTO
   selector: 'app-dbz-main-page',// COMO SE LLAMA EL COMPONENTE EN LA VISTA QUE SE DESEA
@@ -8,10 +9,23 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent {//CLASE A EXPORTAR PARA ACCEDER A ELLA
 
-constructor( public dbzService:DbzService ){
+  constructor(private dbzService: DbzService) {
+
+  }
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id:string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
+
+  OnNewCharacter(character: Character):void{
+    this.dbzService.addCharacter(character);
+  }
 
 
-}
 }
 
 
